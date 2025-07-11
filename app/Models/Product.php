62157a566e1e8e83ceabbc10de28115aa1857b9f
FranckM <votre.ffranckmuzabafr@gmail.com>
2025-07-12 00:40:27 +0100

    modif 2

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
-    
+
     public function orders()
     {
         /**
@@ -21,5 +22,18 @@ class Product extends Model
          */
         return $this->belongsToMany(Order::class)
             ->withPivot('total_price', 'total_quantity');
+    }
+
+    protected function salePrice(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => str_replace('.', ',', $value / 100)
+        );
+    }
+    protected function regularPrice(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => str_replace('.', ',', $value / 100)
+        );
     }
 }
