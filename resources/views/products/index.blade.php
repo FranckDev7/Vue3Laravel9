5d4c76fa50d9faa6ddefb11f9a033749f41f7614
FranckM <votre.ffranckmuzabafr@gmail.com>
2025-07-12 01:30:17 +0100

    modif 3

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -17,7 +17,7 @@
                                     <div class="group relative">
 
                                         <div class="rounded-lg border border-gray-100 shadow-md flex-col overflow-hidden flex w-full relative bg-white max-w-md">
-                                            <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+                                            <a class="rounded-xl flex mx-3  mt-3 overflow-hidden relative" href="#">
                                                 <img class="object-cover" src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="product image" />
                                                 <span class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
                                             </a>
@@ -55,12 +55,8 @@
                                                     <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">5.0</span>
                                                 </div>
                                                 </div>
-                                                <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                                                </svg>
-                                                Add to cart</a
-                                                >
+                                                <!-- button -->
+                                                <add-to-cart></add-to-cart>
                                             </div>
                                         </div>
 
